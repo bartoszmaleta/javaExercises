160d05bf8baf98e0d8844781a33de16c1d671aaf
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-12-14 21:16:02 +0100

    Add simple length count

---
+++ Examples/src/examplesFromCodeCademy/TweetCounter.java
@@ -0,0 +1,16 @@
+
+package examplesFromCodeCademy;
+
+public class TweetCounter {
+  
+	public static void main(String[] args) {
+    
+    String tweet = "Liz Lemon, ninjas are kind of cool... I just dont know any personally. Get on that.";  
+
+    // What's the character count?
+    int lengthString = tweet.length();
+    System.out.println(lengthString);
+    
+  }
+  
+}
