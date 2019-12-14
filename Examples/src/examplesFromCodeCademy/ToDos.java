fe47954ff260ec90efc43e71a6145acac76ef8c8
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-12-14 16:01:31 +0100

    Change components of ArrayLists

--- Examples/src/examplesFromCodeCademy/ToDos.java
+++ Examples/src/examplesFromCodeCademy/ToDos.java
@@ -6,19 +6,33 @@ class ToDos {
     
   public static void main(String[] args) {
     
-    ArrayList<String> toDoList = new ArrayList<String>();
-    String toDo1 = "Water plants";
-    // Add more to-dos here:
-    String toDo2 = "Buy groceries";
-    String toDo3 = "Mop the floor";
+    // Sherlock
+    ArrayList<String> sherlocksToDos = new ArrayList<String>();
     
-    // Add to-dos to toDoList
-    toDoList.add(toDo1);
-    toDoList.add(toDo2);
-    toDoList.add(toDo3);
+    sherlocksToDos.add("visit the crime scene");
+    sherlocksToDos.add("play violin");
+    sherlocksToDos.add("interview suspects");
+    sherlocksToDos.add("solve the case");
+    sherlocksToDos.add("apprehend the criminal");
     
-    System.out.println(toDoList);
-      
+    // Poirot
+    ArrayList<String> poirotsToDos = new ArrayList<String>();
+    
+    poirotsToDos.add("visit the crime scene");
+    poirotsToDos.add("interview suspects");
+    poirotsToDos.add("let the little grey cells do their work");
+    poirotsToDos.add("trim mustache");
+    poirotsToDos.add("call all suspects together");
+    poirotsToDos.add("reveal the truth of the crime");
+    
+    System.out.println("Sherlock's third to-do:");
+    // Print Sherlock's third to-do:
+    System.out.println(sherlocksToDos.get(2));
+    
+    
+    System.out.println("\nPoirot's second to-do:");
+    // Print Poirot's second to-do:
+    System.out.println(poirotsToDos.get(1));
     
   }
   
